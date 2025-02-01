3d46f256f3d5f3007ba1a71a2a1c87096eb98d3e
Gumerov-Vadim <vadim.ugatu@mail.ru>
2025-02-01 18:26:34 +0500

    v2

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const textElement = document.getElementById('text');
-const text = textElement.innerText;
-textElement.innerHTML = '';
-text.split(' ').forEach((word, index) => {
-    const span = document.createElement('span');
-    span.innerText = word;
-    span.classList.add('word');
-    textElement.appendChild(span);
+const text1 = "We're here * to make | healthy * living effortless, | so you can * live longer | and happier *";
+const text2 = "We're here * | to * make | * healthy living | effortless, so you | can * live longer | and happier *";
 
-    switch(index){
-        case 2:
-        case 4:
-        case 10:
-        case 13:
-            const space = document.createElement('span');
-            space.innerText = '⠀';
-            space.classList.add('space');
-            textElement.appendChild(space); 
-    }
-    // if(Math.random() < 0.3){
-    //     const space = document.createElement('span');
-    //     space.innerText = '⠀';
-    //     space.classList.add('space');
-    //     textElement.appendChild(space);
-    // }
-});
+const text1Element = document.getElementById('text1');
+const text2Element = document.getElementById('text2');
+
+const text1ElementHTML = text1Element.innerHTML;
+const text2ElementHTML = text2Element.innerHTML;
+
+const space = document.createElement('span');
+space.innerText = '⠀';
+space.classList.add('space');
+
+function textToHTML(text){
+    return text.split('|').map((words)=>{
+        const textLine = document.createElement('div');
+        textLine.classList.add('text-line');
+        textLine.innerHTML = words.split('*').map((words)=>{    
+            const span = document.createElement('span');
+            span.innerText = words;
+            span.classList.add('word');
+            return span.outerHTML;
+        }).join(`${space.outerHTML}`);
+        return textLine.outerHTML;
+    }).join('');
+}
+
+text1Element.innerHTML = textToHTML(text1);
+text2Element.innerHTML = textToHTML(text2);
